d4425a96fd3c74619af9cca014df529965a604b6
Sebastian_Adell <114223539+sebasadell@users.noreply.github.com>
2022-10-11 11:59:52 +0200

    Add files via upload

--- src/Example18.java
+++ src/Example18.java
@@ -8,48 +8,35 @@ public class Example18 {
         double netPay;
         double tax;
         Scanner inputValue;
-        System.out.println("Enter the price per hour: ");
-        inputValue = new Scanner(System.in);
-        price = inputValue.nextFloat();
 
         System.out.println("Enter the hours: ");
         inputValue = new Scanner(System.in);
         hours = inputValue.nextFloat();
 
-        if (hours<=35) {
-            grossPay = price * hours;
-            if (grossPay <= 500) {
-                netPay = grossPay;
-                tax = 0;
-            } else {
-                double firstSectionGrossPay = grossPay - 500;
-                double secondSectionGrossPay = 0;
-                if (firstSectionGrossPay > 400) {
-                    secondSectionGrossPay = firstSectionGrossPay - 400;
-                }
-                netPay = 500 + (firstSectionGrossPay * 0.25) + (firstSectionGrossPay * 0.45);
-                tax = grossPay - netPay;
-            }
+        System.out.println("Enter the price per hour: ");
+        inputValue = new Scanner(System.in);
+        price = inputValue.nextFloat();
+
+        if (hours > 35) {
+            grossPay = 35 * price + (hours - 35) * price * 1.5;
         }
-        else{
-            double extraHours = hours - 35;
-            grossPay = hours * 35 + extraHours * 1.5;
-            if (grossPay <= 500){
-                netPay = grossPay;
-                tax = 0;
+        else {
+            grossPay = hours * price;
+        }
+        if (grossPay > 500){
+            if (grossPay > 900){
+                tax = 400 * 0.25 + (grossPay - 900) * 0.45;
             }
             else{
-                double firstSectionGrossPay = grossPay - 500;
-                double secondSectionGrossPay = 0;
-                if (firstSectionGrossPay > 400){
-                    secondSectionGrossPay = firstSectionGrossPay - 400;
-                }
-                netPay = 500 + (firstSectionGrossPay * 0.25) + (firstSectionGrossPay * 0.45);
-                tax = grossPay - netPay;
+                tax = (grossPay - 500) * 0.25;
             }
         }
+        else {
+            tax = 0;
+        }
+        netPay = grossPay - tax;
         System.out.println("Gross pay is: " + grossPay + "€");
-        System.out.println("Net pay is: " + netPay + "€");
         System.out.println("Taxes are: " + tax + "€");
+        System.out.println("Net pay is: " + netPay + "€");
     }
 }
